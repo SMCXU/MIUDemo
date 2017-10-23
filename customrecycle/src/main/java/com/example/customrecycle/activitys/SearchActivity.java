59e39d1eb0da75b6b1473c9305c9302ce014e497
U <1539753763@qq.com>
2017-10-23 17:12:13 +0800

    app整合，搜索页相关

--- customrecycle/src/main/java/com/example/customrecycle/activitys/SearchActivity.java
+++ customrecycle/src/main/java/com/example/customrecycle/activitys/SearchActivity.java
@@ -9,43 +9,12 @@ import android.view.View;
 import android.widget.EditText;
 
 import com.example.customrecycle.R;
-import com.example.customrecycle.frame.utils.KeyBoardUtils;
 
 public class SearchActivity extends Activity {
 
-    private Context ctx;
-    private Activity act;
-    private EditText edit;
-    private EditText edit1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        ctx = this;
-        act = this;
-
-        edit = (EditText) this.findViewById(R.id.edit);
-        edit.setInputType(InputType.TYPE_NULL);
-
-        edit1 = (EditText) this.findViewById(R.id.edit1);
-
-        edit.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                new KeyBoardUtils(act, ctx, edit).showKeyboard();
-                return false;
-            }
-        });
-
-        edit1.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                int inputback = edit1.getInputType();
-                edit1.setInputType(InputType.TYPE_NULL);
-                new KeyBoardUtils(act, ctx, edit1).showKeyboard();
-                edit1.setInputType(inputback);
-                return false;
-            }
-        });
     }
 }
